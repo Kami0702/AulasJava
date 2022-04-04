c80992dddcbaa6eb1ac40e586392195756a1ffae
aluno <aluno@DSN1251230707>
2022-04-04 11:36:42 -0300

    commit pessoa e main

---
+++ OrientacaoObjeto/src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		pessoa pessoa1 = new pessoa();
+		
+		pessoa1.nome = "Kamilly";
+        pessoa1.numFigurinhas = 1;
+        
+        pessoa1.recceber(3);
+        
+        System.out.println(pessoa1.nome);
+        System.out.println(pessoa1.numFigurinhas);
+        
+
+	}
+
+}
---
+++ OrientacaoObjeto/src/pessoa.java
@@ -0,0 +1,19 @@
+
+public class pessoa {
+
+  String nome;
+  int numFigurinhas;
+  
+  void recceber(int numFigurinhas) {
+	  this.numFigurinhas += numFigurinhas;
+	  
+  }
+	  
+ 
+  
+  void dar() {
+  
+  
+  
+  }	
+}
