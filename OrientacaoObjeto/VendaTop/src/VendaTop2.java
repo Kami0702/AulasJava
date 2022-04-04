eecbea44ddb679587f0437680b395a967fe7af44
aluno <aluno@DSN1251230707>
2022-04-04 10:47:13 -0300

    orientação objeto

---
+++ OrientacaoObjeto/VendaTop/src/VendaTop2.java
@@ -0,0 +1,27 @@
+
+public class VendaTop2 {
+
+	public static void main(String[] args) {
+		double calça;
+		double moletom;
+		double total;
+		double desco;
+		
+		System.out.println("Digite o primeiro valor: " );
+		
+		System.out.println("Você tem direitoa desconto: " + desco);
+		
+		
+		
+		System.out.println("Digite o segundo valor: " );
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ OrientacaoObjeto/src/Calculadora.java
@@ -0,0 +1,34 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+	double n1 = 5;
+	double n2 = 2;
+	double resulS;
+	double resulSu;
+	double resulM;
+	double resulD;
+	
+	resulS = n1 + n2;
+	System.out.println("O resultado da soma é " + resulS);
+	
+	resulSu = n1 - n2;
+	System.out.println("O resultado da subtração é " + resulSu);
+	
+	resulM = n1 * n2;
+    System.out.println("O resultado da multiplicação é " + resulM);
+    
+    resulD = n1 / n2;
+    System.out.println("O resultado da divisão é " + resulD);
+    
+	
+	
+	
+	
+		
+		
+
+	}
+
+}
+
