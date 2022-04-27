cafb7826bf9ea1cbb2af0f66e94e40df812a170c
aluno <aluno@DSN1251230707>
2022-04-27 07:58:36 -0300

    fundamentos basicos da programaçao

---
+++ FundamentosDeJava/src/main.java
@@ -0,0 +1,133 @@
+                          
+public class main {
+
+	public static void main(String[] args) {
+
+ int x = 100;
+ 
+ double y = 200.3  ;
+ 
+ int a = 0;
+ double b =0;
+ 
+ short z = (short)10000;
+ System.out.println(z);
+ 
+ 
+a = (int)y; //*casting - escalação é quando passa a variavel para outra sem perder o conteudo;
+b = x;  //*casting implicint pega o resto e preenche com zero
+ 
+ System.out.println(a);
+System.out.println(b);
+	
+int idadePessoa = 60;
+
+if(idadePessoa < 18) {
+	System.out.println("Você não pode beber") ;	
+}
+	else if(idadePessoa < 60) {
+		System.out.println("Bora toma uma");
+	}else	{
+		System.out.println("quanto é sua aposentadoria?");	
+		
+ System.out.println("Você tem " +idadePessoa + " anos"); 
+ 
+ 
+} 
+ 
+ //int num = 8; 	
+//switch (num) {
+  //case 1':
+	  //System.out.println("Domimgo");
+	  //break;
+  //case 2:
+	  //System.out.println("Segunda");
+	  //break;
+  //case 3:
+	  //System.out.println("Terça");  
+	  //break;
+  //case 4:
+	  //System.out.println("Quarta");
+	  //break;
+ // case 5:
+ // System.out.println("Quinta");
+        //break;	
+//  case 6:
+	 // System.out.println("Sexta");
+	       // break;	
+ // case 7:
+	 // System.out.println("Sabado");
+	  //      break;	
+ 	//default: 
+ 		//System.out.println("Esse numero não representa um dia da semana");
+ //   }
+
+ //char caractere = 'c';
+
+		 //*switch (caractere) {
+//*case 'a':
+			 //*System.out.println("seu caractere é a");
+	//		 break;
+		// case 'b':
+			// System.out.println("seu caractere é b");
+			// break;
+		// default:
+			 //System.out.println("valor inválido");
+
+
+ //String farol = "vermelho"; 
+ 
+ //switch (farol) {
+ //case "vermelho":
+ //System.out.println("Pare!!");
+ //break;
+ //case "amarelo":
+//System.out.println("Atenção");
+//break;
+ //case "verde":
+//System.out.println("Siga!!");
+//break;
+//default:
+	//System.out.println("valor invalido");
+	 
+
+// }
+	
+//  int x=0
+//while serve para fazer laços repetiçoes		  
+  // while(x<10) {
+	// System.out.println(x);
+//	x=x+1;
+ // }	 
+	  
+	 
+//x=0;	  
+//	  do {
+		 
+//		 System.out.println(x);
+//		 x=x+1;
+//	  }while(x<10);
+
+// System.out.println("Teste " + x);
+Scanner entrada = new Scanner(System.in);
+
+entrada.newtInt();
+
+// ____________________________________________________________________                                
+	 
+  // for(int x=1; x<-10; x*+2) {
+ // System.out.println(x);	
+
+//	}
+   
+for(int x1=0; x1<=10; x1++) {
+ System.out.println("2 x " + x1 + "=" + x1*2 );
+ 
+}
+	
+	
+
+	}
+	
+
+}
