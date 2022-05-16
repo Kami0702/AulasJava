3ff37954b8f31d44c1a6934983f4592d110e98cb
Kami0702 <102962810+Kami0702@users.noreply.github.com>
2022-05-16 08:37:31 -0300

    Update Diamante.java

--- Abstrata/src/Diamante.java
+++ Abstrata/src/Diamante.java
@@ -8,5 +8,5 @@ public class Diamante extends Item {
 		System.out.println("Pegou Diamante");
 	}
 	
-
+//é posssivel aões usando poliformismo do mesmo objeto ou seja o poliformismo serve para fazer diferentes açoes com o mesmo objetos;
 }
